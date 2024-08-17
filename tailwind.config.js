42d85d1b3e4329ccabaac37ce49a27b6b3c345f7
jorganiques <50425083+jorganiques@users.noreply.github.com>
2024-08-17 18:35:38 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        wood: '#DEB887', // Burlywood color
+        darkerwood: '#3b2d1a', // Bistre color
+        darkwood: '#8C6646', // Example wood color
+      },
+    },
+  },
+  plugins: [],
+}
