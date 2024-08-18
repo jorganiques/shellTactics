1adea12edefc0863039f2ac1bc60c7df4a9f1452
jorganiques <50425083+jorganiques@users.noreply.github.com>
2024-08-18 17:38:22 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/shellTactics/',  // Add the correct base path here
   plugins: [react()],
   resolve: {
     alias: {
